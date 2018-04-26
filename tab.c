6893d43cebc466a31608d8664b823b05d2443702
mat <gitmata@hotmail.com>
2018-04-26 10:07:46 -0500

    hola esta es la modificacion

---
+++ tab.c
@@ -0,0 +1,19 @@
+//
+//  nom.c
+//
+//
+//  Created by Ernesto Mata on 12/04/18.
+//
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int x,y,z;
+    
+    printf("atamnlkdljdl");
+    //////jhaajhdjahdj
+    return 0;
+    
+}
