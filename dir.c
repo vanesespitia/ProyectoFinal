1ea8db0dbe7af7790c11c33c14161063fba59a04
karinareyes <karinareyes1295@gmail.com>
2018-04-26 10:12:51 -0500

    Karina

---
+++ dir.c
@@ -0,0 +1,11 @@
+/*******************************************/
+/*Esta funcion hara el direccionamiento y  */
+/*encontrara la ruta mas corta para llegar */
+/*al destino                               */
+/*******************************************/
+
+#include<stdio.h>
+
+void main(){
+printf("Esta funcion realizara el direccionamiento")
+}
