7f3a09b33b20acae92dc5fc1b156588da1966dff
vanesespitia <35779348+vanesespitia@users.noreply.github.com>
2018-05-08 20:41:29 -0500

    Lee si existe archivo

--- top.c
+++ top.c
@@ -3,5 +3,19 @@
 
 int main(void)
 {
-  printf("me toco la topologia. atte: Vanne");
+  FILE *Topologia;
+  char aTopo;
+  
+  printf("me toco la topologia vane :D");
+  aTopo=fopen("topologia.txt","r");
+  if(aTopo==NULL)
+  {
+    printf("No existe la topologia");
+    exit(0);
+  }
+  else
+  {
+    printf("Existe la topologia\n");
+  }
+  
 }
